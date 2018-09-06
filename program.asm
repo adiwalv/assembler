924088a4267fc2ef8331407ef7fcad02c9e4f3a7
Vikas Adiwal <adiwalv@gmail.com>
2018-09-07 05:23:58 +0530

    changed program

--- program.asm
+++ program.asm
@@ -1,10 +1,4 @@
-	section .bss
-	z resb 10
-	f resd 2
-	f resb 3
-	z resd 3
-	f resd 2
-section .data
+	section .data
 	a db "hello",10,10,10,10,10,10,0	
 	c db "jj",10,10,0		
 	b dd 10,20
@@ -12,6 +6,12 @@ section .data
 	t dd 10,12,34,5
 	a db "hel",10,0
 	m dd 2
+	section .bss
+	z resb 10
+	f resd 2
+	f resb 3
+	z resd 3
+	f resd 2
 	section .text
 	global main,printf,scanf,zzz
 main:	mov eax,t
