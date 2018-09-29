951048498448f0726430206e618d846e46ed6af6
Vikas Adiwal <adiwalv@gmail.com>
2018-09-29 17:30:06 +0530

    implementing littable

--- build/definitions.h
+++ build/definitions.h
@@ -41,7 +41,7 @@ int populateLiteralTable(int sym_table_index, int lit_table_index);
 void printSource(char *filename);
 void printSymTab(int sym_table_index);
 void printLiteralTab(int lit_table_index);
-void printImmediateCode(FILE* op, char *filename);
+void printImmediateCode(FILE* op);
 void printErrorList(int error_table_index, int sym_table_index);
 char* makeLittleEndian(char *str);
 
